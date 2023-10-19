d983b9aa21ff0bf80197a53aa9b464078c7a8dbf
Kosgei Kelvin <kosgei@sendyit.com>
2023-10-19 17:08:01 +0300

    moved listener to main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:forced_updates_demo/di/get_it.dart';
 import 'package:forced_updates_demo/features/app_settings/presentation/cubits/cubit/app_version_cubit.dart';
 import 'package:forced_updates_demo/features/food/presentation/pages/meals_page.dart';
+import 'package:native_updater/native_updater.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,7 +33,26 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           brightness: Brightness.dark,
         ),
-        home: const MealsPage(),
+        home: BlocListener<AppVersionCubit, AppVersionState>(
+          listener: (context, state) {
+            state.whenOrNull(loaded: (
+              bool optionalUpdate,
+              bool forceUpdate,
+              String message,
+            ) {
+              if (forceUpdate || optionalUpdate) {
+                NativeUpdater.displayUpdateAlert(
+                  context,
+                  forceUpdate: forceUpdate,
+                  appStoreUrl:
+                      'https://apps.apple.com/us/app/chick-fil-a/id488818252',
+                  iOSDescription: message,
+                );
+              }
+            });
+          },
+          child: const MealsPage(),
+        ),
         themeMode: ThemeMode.dark,
       ),
     );
